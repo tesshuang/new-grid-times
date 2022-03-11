0aa358a3cdaccf8aeef6412c5a0152068eb00267
Tess Huang <tesshuangxj@yahoo.com>
2022-03-11 15:07:16 -0800

    Update avatart position in opinionStory

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { QUERIES } from '../../constants';
+
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
     <a href={`/story/${id}`}>
@@ -17,6 +19,18 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  align-items: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: column;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    flex-direction: row-reverse;
+  }
 `;
 
 const Avatar = styled.img`

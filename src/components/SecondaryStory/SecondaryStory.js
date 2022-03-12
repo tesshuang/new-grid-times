373a6c72444f18dca6a80e2e377fbe97432f9eb1
Tess Huang <tesshuangxj@yahoo.com>
2022-03-11 16:04:34 -0800

    Add elipse for secondaryStory abstract

--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -56,6 +56,10 @@ const Abstract = styled.p`
   grid-area: abstract;
   font-size: 1rem;
   white-space: pre-wrap;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  -webkit-line-clamp: 3;
 `;
 
 export default SecondaryStory;
